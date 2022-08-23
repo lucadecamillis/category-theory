cd485ebafde1655a6eb2c170a674cd442ddd1c7d
Luca De Camillis <lucadecamillis@gmail.com>
2022-08-23 22:10:40 +0200

    Few either tests

--- src/Category.Theory.Tests/EitherUnitTests.cs
+++ src/Category.Theory.Tests/EitherUnitTests.cs
@@ -5,12 +5,26 @@ namespace Category.Theory.Tests;
 public class EitherUnitTests
 {
     [Fact]
+    public void Either_CanCreateRight()
+    {
+        Either<string, int> e = new Right<string, int>(3);
+        Assert.True(e.HasRight());
+        Assert.False(e.HasLeft());
+    }
+
+    [Fact]
+    public void Either_CanCreateLeft()
+    {
+        Either<string, int> e = new Left<string, int>("value");
+        Assert.True(e.HasLeft());
+        Assert.False(e.HasRight());
+    }
+
+    [Fact]
     public void Either_CanSelect()
     {
         Either<Exception, int> entity = new Right<Exception, int>(2);
 
         var stringEntity = entity.Select(e => e.ToString());
-
-        
     }
 }
